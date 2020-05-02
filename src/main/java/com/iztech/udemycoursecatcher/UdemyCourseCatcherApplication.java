47d861d77e6648b116c65ffbcd9bd85a3e0350cd
Bayram Hakan Kocatepe <hakan.kocatepe@bitaksi.com>
2020-05-02 21:43:44 +0300

    initial Commit

---
+++ src/main/java/com/iztech/udemycoursecatcher/UdemyCourseCatcherApplication.java
@@ -0,0 +1,13 @@
+package com.iztech.udemycoursecatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCourseCatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyCourseCatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iztech/udemycoursecatcher/UdemyCourseCatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iztech.udemycoursecatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyCourseCatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
